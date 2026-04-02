a321e90e58766955e259f044083354a19ea44e91
Fransisco <crazyashtro@gmail.com>
2026-04-01 23:11:18 -0400

    Complete OOP Module 8 implementation

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.csc325_oop_designreview_lab;
+import java.util.Scanner;
 
 /**
  *
@@ -11,32 +12,33 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class MainClass {
 
  public static void main(String[] args) {
-		// ToDo 5: Fix the error
 
-		// ToDo 6: Fix the constructor of the Student class
 
-                // Todo 7: Create two classes for Freshman and Senior 
+     //Student std1= new Student("James", 20);
 
-                // ToDo 8: The senior class should have a minimum of 85 credits  
+     Scanner scanner = new Scanner(System.in);
 
-		// ToDo 9: Add a toString method for the Student class
-		// ToDo 10: Add a toString method for the Freshman class
+     // Create Freshman
+     Freshman std1 = new Freshman("James", (short)20, "NY", 0.0, 12);
 
-		//Student std1= new Student("James", 20);
-                // ToDo 11: Add a toString method for the Senior class
+     // Create Senior
+     Senior std2 = new Senior("John", (short)30, "CA", 0.0, 90);
 
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
 
-                Senior std2 = new Student("John", 30, 90);
+     // User input GPA
+     System.out.print("Enter GPA for Freshman: ");
+     double gpa1 = scanner.nextDouble();
+     std1.setGpa(gpa1);
 
-		// ToDo 12: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
+     System.out.print("Enter GPA for Senior: ");
+     double gpa2 = scanner.nextDouble();
+     std2.setGpa(gpa2);
 
 		System.out.println(std1);
 
-                System.out.println(std2);
+        System.out.println(std2);
 
-		// ToDo 13: add comments and explain your code
+
 
 	}
 
